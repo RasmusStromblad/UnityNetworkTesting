fd2d3a717596f2b9086d7eb290f3090eca7e98fa
RasmusStromblad <56510265+RasmusStromblad@users.noreply.github.com>
2020-01-25 22:56:51 +0100

    Added files
    Added the pre-written files from my local disc. These contains the basics of networking with unity. Players can create an account and join a room together

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,122 @@
+﻿using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField] private GameObject findOpponentPanel = null;
+    [SerializeField] private GameObject waitingStatusPanel = null;
+    [SerializeField] private TextMeshProUGUI waitingStatusText = null;
+
+    private bool isConnecting = false;
+
+    // Needed to chech if users using the same version of the game
+    private const string GameVersion = "0.1";
+    // Needed to chech if the amount of players needed to start the game is fullfilled
+    private const int MaxPlayersPerRoom = 2;
+
+
+    private void Awake()
+    {
+        // Make sure that the same things happens to all players in a room. Example: Load a scene for everyone
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public void FindOpponent()
+    {
+        isConnecting = true;
+
+        findOpponentPanel.SetActive(false);
+        waitingStatusPanel.SetActive(true);
+
+        waitingStatusText.text = "Searching...";
+
+        // If you are connected
+        if (PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+        else
+        {
+            // Connect to server using specified settings
+            PhotonNetwork.GameVersion = GameVersion;
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master");
+
+        if (isConnecting)
+        {
+            PhotonNetwork.JoinRandomRoom();
+        }
+    }
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        waitingStatusPanel.SetActive(false);
+        findOpponentPanel.SetActive(true);
+
+        Debug.Log($"Disconnected due to: {cause}"); 
+    }
+
+    // If there are no random rooms to join
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("No clients are waiting for an opponent, creating a new room");
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = MaxPlayersPerRoom });
+    }
+
+    // What happens when we join a room
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Client successfully joined a room");
+
+        int playerCount = PhotonNetwork.CurrentRoom.PlayerCount;
+
+        if(playerCount != MaxPlayersPerRoom)
+        {
+            waitingStatusText.text = "Waiting for opponent";
+            Debug.Log("Client is waiting for an opponent");
+        }
+        else
+        {
+            waitingStatusText.text = "Opponent found";
+            Debug.Log("Match is ready to begin");
+        }
+    }
+
+    // What happens when player enters your room
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        if(PhotonNetwork.CurrentRoom.PlayerCount == MaxPlayersPerRoom)
+        {
+            // No new players can join
+            PhotonNetwork.CurrentRoom.IsOpen = false;
+
+            Debug.Log("Match is ready to begin");
+            waitingStatusText.text = "Opponent found";
+
+            PhotonNetwork.LoadLevel("Scene_1");
+        }
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
